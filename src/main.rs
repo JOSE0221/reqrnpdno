5fd58397076d79eb746028c6894871c8e04b02ec
JoseLuis <111091336+JOSE0221@users.noreply.github.com>
2024-12-09 09:47:43 -0600

    a
    a

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-use reqrnpdno::{extractora,parameters::Parametros};
+use reqrnpdno::{extractora, parameters::Parametros, utilidades};
 
-fn main () {
+fn main() {
+    // Aquí se define la ruta en donde se guardarán los datos.
+    // En este caso es un directorio llamado "30-nov-2021" que aún no existe pero que crearemos en el directorio "salida".
+    // Para crearlo usamos la función de ayuda "crear_directorio()" para la cual el primer parámetro es la ruta objetivo y el segundo parámetro es el nombre del directorio a crear.
+    let ruta_base = "./salida/";
+    let nombre_directorio = "30-nov-2021";
 
-    // Aquí se define la ruta en donde se guardarán los datos. En este caso es un archivo de tipo JSON llamado datos.json
-    let rutam = "./datos.json".to_string();
+    // Verificar si la ruta base existe
+    if !std::path::Path::new(ruta_base).exists() {
+        std::fs::create_dir(ruta_base).expect("No se pudo crear el directorio base 'salida'");
+    }
 
-    // Aquí creamos la estructura de los parámetros necesarios para realizar la petición. Esta estructura es la que se necesita modificar si quieres aplicar algún filtro. Si no quieres filtrar no es necesario modificar la estructura. 
-    let mut parametros = Parametros::new();
+    let ruta_salida = utilidades::crear_directorio(ruta_base, nombre_directorio).unwrap();
+    println!("Directorio creado en: {:?}", ruta_salida);
 
-    // Aquí se modifican los valores de los parámetros para aplicar algunos filtros. 
-    // En este caso se coloca el valor "7" en el campo "id_estatus_victima" el cual corresponde a "PERSONAS DESAPARECIDAS Y NO LOCALIZADAS". 
-    // También cambiamos el parámetro "titulo" y colocamos el título. 
-    // Además usamos los parámetros "fecha_inicio" y "fecha_fin" para limitar la búsqueda a un rango de fechas.
-    parametros.id_estatus_victima = "7".to_string();
-    parametros.titulo = "PERSONAS DESAPARECIDAS Y NO LOCALIZADAS".to_string();
-    parametros.fecha_inicio = "2000-01-01".to_string();
-    parametros.fecha_fin = "2021-08-20".to_string();
+    // Aquí creamos la estructura de los parámetros necesarios para realizar la petición.
+    // Esta estructura es la que se necesita modificar si quieres aplicar algún filtro.
+    // Si no quieres filtrar no es necesario modificar la estructura.
+    let parametros = Parametros::new();
 
-    // Por último, utilizamos la función de alto nivel "extraer" para obtener nuestros datos.
-    extractora::extraer(&parametros, &rutam).unwrap();
+    // Para este caso no modificaremos la estructura con los parametros.
 
+    // Por último, utilizamos la función de alto nivel "extraer_todo_iterando" para obtener nuestros datos.
+    extractora::extraer_todo_iterando(&parametros, &ruta_salida).unwrap();
 }
-
